51eb2f8ecfc88cffac756ecf44c670a47b434478
Edgaras Ausvicas <eda@spiir.dk>
2018-11-28 15:28:24 +0100

    Update USERHASH

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ CLIENT_SECRET = "<enter your client secret here>"
 API_BASE = "https://api.nordicapigateway.com/v1/"
 
 SECRET_KEY = "<some random string>"  # for local session storage
-USERHASH = "demo"
+USERHASH = "test-user-id"
